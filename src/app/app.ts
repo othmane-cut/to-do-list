578dd90ef9e54b0e8cecbc05ff2ba03d7f75c93b
othmane <lheothmane@gmail.com>
2025-10-27 10:36:40 +0100

    creat user component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { User } from "./user/user";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header],
+  imports: [Header, User],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
