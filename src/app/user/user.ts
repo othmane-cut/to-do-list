f80db44fad30c93b65d19dfabefa02a530f4dfcc
othmane <lheothmane@gmail.com>
2025-10-28 00:37:52 +0100

    use input with single

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, computed, Input,input } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users'; 
 
 const randomeUserIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -9,12 +9,16 @@ const randomeUserIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrls: ['./user.css']
 })
 export class User {
-  @Input() avatar!: string;
-  @Input() name!: string;
+  //@Input({required: true}) avatar!: string;
+  //@Input({required: true}) name!: string;
+  avatar = input.required<string>();
+  name = input.required<string>();
 
-  get pathimage(): string {
-    return `../assets/users/${this.avatar}`;
-  }
+  pathimage = computed
+  (() => `../assets/users/${this.avatar()}`);
+  // get pathimage(): string {
+  //   return `../assets/users/${this.avatar}`;
+  // }
 
   onUserClick() {
   
