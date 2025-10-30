f9064abf965c5327a72bb0b8134ed4cd50e4502f
othmane <lheothmane@gmail.com>
2025-10-30 13:59:46 +0100

    configurable compenent

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,19 +3,25 @@ import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
 import { User } from "./user/user";
 import { DUMMY_USERS } from './dummy-users';
+import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [Header, User],
+  imports: [Header, User,Tasks],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
+
 export class App {
   users = DUMMY_USERS;
-
+  selectedUserId = 'u5';
+  
+  get userSelected() {
+    return this.users.find(user => user.id === this.selectedUserId)!;
+  }
 
   onSelectUser(userId: string) {
-    console.log('Selected user ID: ' + userId);
+    this.selectedUserId = userId;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,11 @@ import { appConfig } from './app/app.config';
 import { App } from './app/app';
 import { Header } from './app/header/header';
 import { User } from './app/user/user';
+import { Tasks } from './app/tasks/tasks';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
 
   bootstrapApplication(Header)
   bootstrapApplication(User)
+  bootstrapApplication(Tasks)
