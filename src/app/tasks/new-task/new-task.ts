10c80555838b470f012301f25f7f13b9a690bab7
othmane <lheothmane@gmail.com>
2025-11-03 10:14:22 +0100

    use directive for get data from iput

--- src/app/tasks/new-task/new-task.ts
+++ src/app/tasks/new-task/new-task.ts
@@ -1,15 +1,22 @@
-import { Component,Output,EventEmitter } from '@angular/core';
+import { Component,Output,EventEmitter, signal } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 
 @Component({
   selector: 'app-new-task',
-  imports: [],
+  imports: [BrowserModule, FormsModule],
   templateUrl: './new-task.html',
-  styleUrl: './new-task.css'
+  styleUrls: ['./new-task.css']
 })
 export class NewTask {
 
   @Output() canceled = new EventEmitter<void>();
+  summary = signal("");
+  title = signal("");
+
+  dueDate = signal("");
 
   onCancel() {
     this.canceled.emit();
