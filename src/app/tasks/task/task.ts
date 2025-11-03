15fbf8f01b53d46bcdc0949d0153a267ddba4fef
othmane <lheothmane@gmail.com>
2025-11-03 11:36:51 +0100

    use pipes for fromat date

--- src/app/tasks/task/task.ts
+++ src/app/tasks/task/task.ts
@@ -1,11 +1,13 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { task } from './task.model';
 import { Card } from "../../shareUI/card/card";
+import { DatePipe } from '@angular/common';
+
 
 
 @Component({
   selector: 'app-task',
-  imports: [Card],
+  imports: [Card, DatePipe],
   templateUrl: './task.html',
   styleUrl: './task.css'
 })
